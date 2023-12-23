c0002994fdd35cf9a7281dd92e3a95b0ea929fc7
Abhinav Reddy <areddyyt@gmail.com>
2023-12-23 22:51:47 +0530

    Make for loop that adds letter from back to start for new string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    reversedString = '';
+    for(let i = string.length + 1; i > -1; i--){
+        reversedString += string.charAt(i);
+    }
 
+    return reversedString;
 };
 
 // Do not edit below this line
