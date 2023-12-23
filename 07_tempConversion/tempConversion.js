9049bae2dd0d283cfa364089cea81d6aeb9a4b66
Abhinav Reddy <areddyyt@gmail.com>
2023-12-24 00:30:29 +0530

    Use Math.round to round to one decimal place in answer

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degFahrenheit) {
+  return Math.round((((degFahrenheit-32)*5)/9)*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degCelsius) {
+  return Math.round((((degCelsius*9)/5)+32)*10)/10;
 };
 
 // Do not edit below this line
