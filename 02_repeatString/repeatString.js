689bed6f2532061311f02c4a288a2e2d73d074a8
Abhinav Reddy <areddyyt@gmail.com>
2023-12-23 22:36:21 +0530

    Create initial logic to repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatNumber) {
+    let repeatedString = '';
+    for(let i = 0; i < repeatNumber; i++){
+        repeatedString += string;
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
