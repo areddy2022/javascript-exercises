ca879968492750143df120372427c362f8254e32
Abhinav Reddy <areddyyt@gmail.com>
2023-12-23 23:54:07 +0530

    Add for loop to iterate between and including both arguments and sum them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
 
+    for(; startNum < endNum + 1; startNum++){
+        sum += startNum;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
