87cdd9fa189f7cf9c1c6af9d7179a7a4f604126e
Abhinav Reddy <areddyyt@gmail.com>
2023-12-24 00:02:43 +0530

    Check if type is number or non-negative

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
 const sumAll = function(startNum, endNum) {
     let sum = 0;
 
+    if(typeof startNum != "number" || typeof endNum != "number" || 
+    startNum < 0 || endNum < 0){
+        return "ERROR";
+    }
+
     for(; startNum < endNum + 1; startNum++){
         sum += startNum;
     }
