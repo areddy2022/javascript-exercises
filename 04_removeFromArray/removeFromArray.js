fb736fefd5f86fe43215b3672188bc1009c9dde8
Abhinav Reddy <areddyyt@gmail.com>
2023-12-23 23:03:20 +0530

    Iterate through the array and push to new array except for matching argument

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, arg1) {
+    ansArr = [];
+    for(index in arr){
+        if(arr[index] !== arg1){
+            ansArr.push(arr[index]);
+        }
+    }
+    return ansArr;
 };
 
 // Do not edit below this line
