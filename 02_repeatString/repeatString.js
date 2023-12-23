e0b6d615e64110791e0e1a0af25cfe1ffc67e2ad
Abhinav Reddy <areddyyt@gmail.com>
2023-12-23 22:46:54 +0530

    Add check for negative repeated numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,12 @@
 const repeatString = function(string, repeatNumber) {
+    if(repeatNumber < 0) {
+        return "ERROR";
+    }
     let repeatedString = '';
     for(let i = 0; i < repeatNumber; i++){
         repeatedString += string;
     }
-    return repeatedString
+    return repeatedString;
 };
 
 // Do not edit below this line
