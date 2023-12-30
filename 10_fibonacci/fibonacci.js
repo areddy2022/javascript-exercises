88d95a7b9ef6ce4a1e878e3bfde12b0d0e7d1e84
Abhinav Reddy <areddyyt@gmail.com>
2023-12-30 21:28:57 +0530

    Refactor to iteratively solve with array

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,33 +1,16 @@
 const fibonacci = function(i) {
-    i = Number(i);
-    let first = 1;
-    let second = 1;
-    let placehold;
-    let sum = 0;
-    let sumarr = [0];
+    i = parseInt(i);
+    let sumarr = [0, 1];
     if(i < 0){
         return "OOPS";
     }
-    switch(i){
-        case 0:
-            return sumarr[0];
-        case 1:
-            return 1;
-        case 2:
-            return 1;
+    for(let x = 2; x < i + 1; x++){
+        sumarr[x] = sumarr[x - 1] + sumarr[x - 2];
+        console.log(sumarr[x]);
     }
-    sumarr = [1, 1];
-    for(let x = 0; x < i; x++){
-        sum = first + second;
-        placehold = second + first;
-        second = first;
-        first = placehold;
-        sumarr.push(sum);
-    }
-    return sumarr[i - 1];
+    console.log(sumarr[i])
+    return sumarr[i];
 };
-
-fibonacci("1")
 
 // Do not edit below this line
 module.exports = fibonacci;
