11d38d9c0099f9ce7e2fb77622f43d531e29ae52
Abhinav Reddy <areddyyt@gmail.com>
2023-12-30 21:01:40 +0530

    Create function to construct processed word without spaces, punctuation, or capitalization

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,14 +1,22 @@
 const palindromes = function (word) {
-    let procWord = word.split("");
+    let procWord = wordProcessor(word);
     for(x in procWord){
         if(procWord[x] != procWord[procWord.length - 1 - x]){
             return false;
         }
-        return true;
     }
+    return true;
 };
 
-palindromes("racecar");
+function wordProcessor(word){
+    newword = [];
+    for(each in word){
+        if(word[each].toUpperCase() != word[each].toLowerCase() || !isNaN(parseInt(word[each]))){
+            newword.push(word[each].toLowerCase());
+        }
+    }
+    return newword;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
