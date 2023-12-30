e67791f2df317d44a889a4c5d3be44353c44284a
Abhinav Reddy <areddyyt@gmail.com>
2023-12-30 22:15:20 +0530

    Use methods to create lists for alive and dead people and compare ages

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,43 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(list) {
+    const BASE_YEAR = 2023;
+    let dead = list.filter(person => "yearOfDeath" in person);
+    let alive = list.filter(person => !("yearOfDeath" in person));
+    let unsortedDead = dead.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth));
+    let unsortedAlive = alive.sort((a, b) => b.yearOfBirth - a.yearOfBirth);
+    let sortedDead = unsortedDead.reverse();
+    let sortedAlive = unsortedAlive.reverse();
+    const deadPerson = sortedDead[0];
+    const alivePerson = sortedAlive[0];
+    if(alivePerson == undefined){
+        return deadPerson;
+    }
+    else if(deadPerson.yearOfDeath - deadPerson.yearOfBirth > 2023 - alivePerson.yearOfBirth){
+        return deadPerson;
+    }
+    else{
+        return alivePerson;
+    }
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+  console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
