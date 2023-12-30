aff89f3fb86978066df9bba2fb55d931acde10cc
Abhinav Reddy <areddyyt@gmail.com>
2023-12-30 13:01:29 +0530

    Create base logic to check palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let procWord = word.split("");
+    for(x in procWord){
+        if(procWord[x] != procWord[procWord.length - 1 - x]){
+            return false;
+        }
+        return true;
+    }
 };
+
+palindromes("racecar");
 
 // Do not edit below this line
 module.exports = palindromes;
